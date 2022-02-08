1d6d0a948b656d2b01002c7bf73a00fa736e2bb6
Magnus <magn902m@stud.kea.dk>
2022-02-08 16:16:32 +0100

    added speed and sound playing

--- typewriter.js
+++ typewriter.js
@@ -16,6 +16,7 @@ const typeReturn = document.querySelector("#typereturn");
 let i;
 let maxLen;
 let speed;
+let speedSet = 1000;
 
 function load() {
   console.log("load");
@@ -35,12 +36,37 @@ function initTextLoop() {
   textLoop();
 }
 
+document.querySelector("html").addEventListener("click", playSound);
+
+function playSound() {
+  if (string.indexOf(" ") === " ") {
+    console.log("Space");
+    typeSpace.play();
+  } else {
+    console.log("Type");
+    typeKey1.play();
+  }
+}
+
+function setUpSpeed() {
+  speed = Math.floor(Math.random() * 3) + 1;
+  if (speed === 1) {
+    speedSet = 1000;
+  } else if (speed === 2) {
+    speedSet = 2000;
+  } else if (speed === 2) {
+    speedSet = 3000;
+  }
+}
+
 function textLoop() {
   console.log("i", i);
   i++;
   if (i < string.length) {
     document.querySelector(".typewritten").innerHTML += string.charAt(i);
-    setTimeout(textLoop, 1000);
+    setUpSpeed();
+    setTimeout(textLoop, speedSet);
+    playSound();
   } else {
     console.log("I am done");
     // textLoopDel();
